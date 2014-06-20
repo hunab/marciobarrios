3cf6692ba33ff1e9a224de19743ce47dca53486e
Marcio Barrios <marciobarrios@gmail.com>
2014-06-20 20:59:08 +0200

    Adjusted title effect

--- s.js
+++ s.js
@@ -2,8 +2,8 @@
 // It waits till the background image is loaded and then adds the class "ready" to <body>
 (function(app, $, undefined) {
     //public vars
-    app.IMG          = "img/bg.jpg"
-    app.readyClass   = "ready"
+    app.IMG        = "img/bg.jpg"
+    app.readyClass = "ready"
 
     // private methods
     function onLoadImage() { document.body.className = app.readyClass }
@@ -49,29 +49,29 @@ if (/(MSIE [7-9]\.|Opera.*Version\/(10\.[5-9]|(11|12)\.)|Chrome\/([1-9]|10)\.|Ve
   var ss_set={'github octocat':'\uF671','googleplus':'\uF613','foursquare':'\uF690','thumbs up':'\uD83D\uDC4D','wordpress':'\uF621','posterous':'\uF623','pinterest':'\uF650','instagram':'\uF641','linkedin':'\uF612','thumbsup':'\uD83D\uDC4D','dribbble':'\uF660','facebook':'\uF610','envelope':'\u2709','rss feed':'\uDB8C\uDC20','behance':'\uF661','twitter':'\uF611','octocat':'\uF671','youtube':'\uF630','google+':'\uF613','spotify':'\uF6B1','approve':'\uD83D\uDC4D','last fm':'\uF6B2','blogger':'\uF622','paypal':'\uF680','flickr':'\uF640','github':'\uF670','tumblr':'\uF620','lastfm':'\uF6B2','email':'\u2709','vimeo':'\uF631','skype':'\uF6A0','mail':'\u2709','like':'\uD83D\uDC4D','rdio':'\uF6B0','rss':'\uE310'};
 
   if (typeof ss_icons !== 'object' || typeof ss_icons !== 'object') {
-    var ss_icons = ss_set; 
+    var ss_icons = ss_set;
     var ss_keywords = [];
     for (var i in ss_set) { ss_keywords.push(i); };
   } else {
     for (var i in ss_set) { ss_icons[i] = ss_set[i]; ss_keywords.push(i); }
   };
-  
+
   if (typeof ss_legacy !== 'function') {
 
     /* domready.js */
     !function(a,b){typeof module!="undefined"?module.exports=b():typeof define=="function"&&typeof define.amd=="object"?define(b):this[a]=b()}("ss_ready",function(a){function m(a){l=1;while(a=b.shift())a()}var b=[],c,d=!1,e=document,f=e.documentElement,g=f.doScroll,h="DOMContentLoaded",i="addEventListener",j="onreadystatechange",k="readyState",l=/^loade|c/.test(e[k]);return e[i]&&e[i](h,c=function(){e.removeEventListener(h,c,d),m()},d),g&&e.attachEvent(j,c=function(){/^c/.test(e[k])&&(e.detachEvent(j,c),m())}),a=g?function(c){self!=top?l?c():b.push(c):function(){try{f.doScroll("left")}catch(b){return setTimeout(function(){a(c)},50)}c()}()}:function(a){l?a():b.push(a)}})
-    
+
     var ss_legacy = function(node) {
-      
+
       if (!node instanceof Object) return false;
-      
+
       if (node.length) {
         for (var i=0; i<node.length; i++) {
           ss_legacy(node[i]);
         }
         return;
       };
-      
+
       if (node.value) {
         node.value = ss_liga(node.value);
       } else if (node.nodeValue) {
@@ -79,9 +79,9 @@ if (/(MSIE [7-9]\.|Opera.*Version\/(10\.[5-9]|(11|12)\.)|Chrome\/([1-9]|10)\.|Ve
       } else if (node.innerHTML) {
         node.innerHTML = ss_liga(node.innerHTML);
       }
-        
+
     };
-    
+
     var ss_getElementsByClassName = function(node, classname) {
       var a = [];
       var re = new RegExp('(^| )'+classname+'( |$)');
@@ -90,14 +90,14 @@ if (/(MSIE [7-9]\.|Opera.*Version\/(10\.[5-9]|(11|12)\.)|Chrome\/([1-9]|10)\.|Ve
           if(re.test(els[i].className))a.push(els[i]);
       return a;
     };
-    
+
     var ss_liga = function(that) {
       var re = new RegExp(ss_keywords.join('|').replace(/[-[\]{}()*+?.,\\^$#\s]/g, "\\$&"),"gi");
-      return that.replace(re, function(v) { 
+      return that.replace(re, function(v) {
         return ss_icons[v.toLowerCase()];
       });
     };
-    
+
     ss_ready(function() {
       if (document.getElementsByClassName) {
         ss_legacy(document.getElementsByClassName('ss-icon'));
@@ -105,7 +105,7 @@ if (/(MSIE [7-9]\.|Opera.*Version\/(10\.[5-9]|(11|12)\.)|Chrome\/([1-9]|10)\.|Ve
         ss_legacy(ss_getElementsByClassName(document.body, 'ss-icon'));
       }
     });
-  
+
   }
-  
+
 };
